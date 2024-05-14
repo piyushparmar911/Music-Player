bada37e6a8d97d967790d1b3c7ae4af59a1ce35e
piyushparmar911 <prparmar911@gmail.com>
2024-05-14 23:46:54 +0530

    musicitem updated

--- src/components/MusicItem.js
+++ src/components/MusicItem.js
@@ -3,15 +3,17 @@ import React, { Component } from 'react'
 export class MusicItem extends Component {
   render() {
     return (
-      <div>
-        <div className="card" style={{width: "18rem"}}>
-  <img className="card-img-top" src="" alt="Card image cap" />
+        <>
+      <div className='my-3 mx-2'>
+        <div className="card " style={{width: "18rem"}}>
+  <img className="card-img-top" src={{}} alt="Card image cap"/>
   <div className="card-body">
-    <h5 className="card-title">{this.title}</h5>
-    <p className="card-text">{this.description}</p>
+    <h5 className="card-title">{this.props.title}</h5>
+    <p className="card-text">{this.props.description}</p>
   </div>
 </div>
       </div>
+        </>
     )
   }
 }
