d52259e6635d302b6460b12863784592663d40f7
piyushparmar911 <prparmar911@gmail.com>
2024-05-14 23:12:33 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import Navbar from '/Components/Navbar';
+import React, { Component } from 'react'
+import Navbar from './components/Navbar'
 
-function App() {
-  return (
-    <>
-    <Navbar />
-    </>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div>
+  <Navbar/>        
+      </div>
+)
+  }
 }
 
-export default App;
+export default App
