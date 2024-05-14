7860f6b3d2b29811e576d499c8115b88a75487bb
piyushparmar911 <prparmar911@gmail.com>
2024-05-14 22:59:13 +0530

    updated

---
+++ src/components/MusicItem.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class MusicItem extends Component {
+  render() {
+    return (
+      <div>
+        <div className="card" style={{width: "18rem"}}>
+  <img className="card-img-top" src="" alt="Card image cap" />
+  <div className="card-body">
+    <h5 className="card-title">{this.title}</h5>
+    <p className="card-text">{this.description}</p>
+  </div>
+</div>
+      </div>
+    )
+  }
+}
+
+export default MusicItem
