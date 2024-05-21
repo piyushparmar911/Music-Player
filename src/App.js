7b0edf0c566b01aece5c661020083a6c6bb29d7d
piyushparmar911 <prparmar911@gmail.com>
2024-05-21 10:13:09 +0530

    progessbar updted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import Music from "./components/Music";
-
+import ProgressBar from "@ramonak/react-progress-bar";
 export class App extends Component {
   constructor ()
   {
@@ -13,13 +13,26 @@ export class App extends Component {
   updateserchText = (newserchText) => {
     this.setState({searchText: newserchText});
   }
+
+  ProgressBar = (completed) => {
+    this.setState({completed: completed});
+  }  
+
  render() {
     return (
       <div className=" text-white" style={{background: "black"}}> 
         <Navbar  searchText={this.state.searchText} onSearch={this.updateserchText}/>
+  <ProgressBar
+  completed={this.state.completed}
+  height="1px"
+  bgColor="aqua"
+  borderRadius="0px"
+  labelSize="0px"
+  baseBgColor="black"
+/>
         <div className=" d-flex">
 
-          <Music title="Songs" searchText={this.state.searchText}/>
+          <Music title="Songs" ProgressBar={this.ProgressBar} searchText={this.state.searchText}/>
         </div>
       </div>
     );
