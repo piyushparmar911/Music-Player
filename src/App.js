8e4c904be14a6ea290e371d3461eba849802169f
piyush parmar <145854272+piyushparmar911@users.noreply.github.com>
2024-05-21 10:33:15 +0530

    components updated

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Music from "./components/Music";
+import ProgressBar from "@ramonak/react-progress-bar";
+export class App extends Component {
+  constructor ()
+  {
+    super();
+    this.state = {
+      searchText: "Arijit Singh"
+    };
+  }
+  updateserchText = (newserchText) => {
+    this.setState({searchText: newserchText});
+  }
+
+  ProgressBar = (completed) => {
+    this.setState({completed: completed});
+  }  
+
+ render() {
+    return (
+      <div className=" text-white" style={{background: "black"}}> 
+        <Navbar  searchText={this.state.searchText} onSearch={this.updateserchText}/>
+  <ProgressBar
+  completed={this.state.completed}
+  height="1px"
+  bgColor="aqua"
+  borderRadius="0px"
+  labelSize="0px"
+  baseBgColor="black"
+/>
+        <div className=" d-flex">
+
+          <Music title="Songs" ProgressBar={this.ProgressBar} searchText={this.state.searchText}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
